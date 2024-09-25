b8d25986db12d72994b99bc7dd39e08f316c4641
Helzoph <50453568+Helzoph@users.noreply.github.com>
2024-09-25 16:28:13 +0800

    init

---
+++ .config/fish/config.fish
@@ -0,0 +1,65 @@
+# 不显示 Fish 启动欢迎信息
+set -g fish_greeting
+
+# PATH
+set -x GO_BIN_PATH $HOME/go/bin
+set -x RUST_BIN_PATH $HOME/.cargo/bin
+set -x PATH $GO_BIN_PATH $RUST_BIN_PATH $PATH
+
+
+# zellij
+if set -q ZELLIJ
+else
+  zellij
+end
+
+
+# starship
+starship init fish | source
+
+
+# alias
+## system
+alias mkdir 'mkdir -pv'
+alias scstart 'sudo systemctl start'
+alias scstop 'sudo systemctl stop'
+alias screstart 'sudo systemclt restart'
+alias scenabel 'sudo systemctl enable'
+alias scdisable 'sudo systemctl disable'
+alias screload 'sudo systemctl reload'
+
+## sudo
+alias apti='sudo apt install'
+alias aptiy='sudo apt install -y'
+alias aptud='sudo apt update'
+alias aptug='sudo apt upgrade'
+
+## docker compose
+alias dco 'docker-compose'
+alias dcup 'docker-compose up'
+alias dcupd 'docker-compose up -d'
+alias dcdn 'docker-compose down'
+
+## golang
+alias gor='go run'
+alias gob='go build'
+alias got='go test'
+alias gog='go get'
+alias gom='go mod'
+alias gomi='go mod init'
+alias gomt='go mod tidy'
+
+## go-zero goctl
+alias genapi='goctl api go -api *api -dir ../ --style=gozero'
+alias genmod='goctl model mysql ddl -src *.sql -dir ./'
+alias genmodc='goctl model mysql ddl -src *.sql -dir ./ -c'
+
+## ptyhon
+### venv
+alias pyvenv='python -m venv venv'
+alias scven='source venv/bin/activate.fish'
+
+
+if status is-interactive	
+    # Commands to run in interactive sessions can go here
+end
---
+++ install.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# starship config
+if [ ! -d ~/.config ]; then
+    mkdir ~/.config
+fi
+cp ./.config/starship.toml ~/.config/starship.toml
+
+# fish config
+if [ ! -d ~/.config/fish ]; then
+    mkdir ~/.config/fish
+fi
+cp ./.config/fish/config.fish ~/.config/fish/config.fish
+
+# git config
+cp .gitconfig ~/.gitconfig
+
+# zellij config
+if [ ! -d ~/.config/zellij ]; then
+    mkdir -p ~/.config/zellij
+fi
+cp ./.config/zellij/config.kdl ~/.config/zellij/config.kdl
