3f23d27bb8c18690d3e21e91f759b40f30d91635
Helzoph <50453568+Helzoph@users.noreply.github.com>
2024-09-26 10:56:49 +0800

    update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,15 @@
 # debug
 set -x debug true
 
+
 # 不显示 Fish 启动欢迎信息
 set -g fish_greeting
 
+
 # PATH
-set -x GO_BIN_PATH $HOME/go/bin
-set -x RUST_BIN_PATH $HOME/.cargo/bin
-set -x PATH $GO_BIN_PATH $RUST_BIN_PATH $PATH
+set -gx GO_BIN_PATH $HOME/go/bin
+set -gx RUST_BIN_PATH $HOME/.cargo/bin
+set -gx PATH $GO_BIN_PATH $RUST_BIN_PATH $PATH
 
 
 # zellij
@@ -22,8 +24,7 @@ starship init fish | source
 
 
 # alias
-## system
-alias mkdir 'mkdir -pv'
+## systemctl
 alias scstart 'sudo systemctl start'
 alias scstop 'sudo systemctl stop'
 alias screstart 'sudo systemclt restart'
@@ -31,11 +32,11 @@ alias scenabel 'sudo systemctl enable'
 alias scdisable 'sudo systemctl disable'
 alias screload 'sudo systemctl reload'
 
-## sudo
-alias apti='sudo apt install'
-alias aptiy='sudo apt install -y'
-alias aptud='sudo apt update'
-alias aptug='sudo apt upgrade'
+## apt
+alias apti 'sudo apt install'
+alias aptiy 'sudo apt install -y'
+alias aptud 'sudo apt update'
+alias aptug 'sudo apt upgrade'
 
 ## docker compose
 alias dco 'docker-compose'
@@ -44,23 +45,39 @@ alias dcupd 'docker-compose up -d'
 alias dcdn 'docker-compose down'
 
 ## golang
-alias gor='go run'
-alias gob='go build'
-alias got='go test'
-alias gog='go get'
-alias gom='go mod'
-alias gomi='go mod init'
-alias gomt='go mod tidy'
+alias gor 'go run'
+alias gob 'go build'
+alias got 'go test'
+alias gog 'go get'
+alias gom 'go mod'
+alias gomi 'go mod init'
+alias gomt 'go mod tidy'
 
 ## go-zero goctl
-alias genapi='goctl api go -api *api -dir ../ --style=gozero'
-alias genmod='goctl model mysql ddl -src *.sql -dir ./'
-alias genmodc='goctl model mysql ddl -src *.sql -dir ./ -c'
+alias genapi 'goctl api go -api *api -dir ../ --style=gozero'
+alias genmod 'goctl model mysql ddl -src *.sql -dir ./'
+alias genmodc 'goctl model mysql ddl -src *.sql -dir ./ -c'
 
 ## ptyhon
 ### venv
-alias pyvenv='python -m venv venv'
-alias scven='source venv/bin/activate.fish'
+alias pyvenv 'python -m venv venv'
+alias scven 'source venv/bin/activate.fish'
+
+## system
+### ls
+alias l 'ls -lFh'
+alias la 'ls -lFhA'
+alias ll 'ls -l'
+### file
+alias cp 'cp -iv'
+alias mv 'mv -iv'
+alias rm 'rm -iv'
+alias mkdir 'mkdir -pv'
+### find & grep
+alias fd 'find . -type d -name'
+alias ff 'find . -type f -name'
+alias grep 'grep --color=auto'
+alias sgrep 'grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS}'
 
 
 if status is-interactive	
